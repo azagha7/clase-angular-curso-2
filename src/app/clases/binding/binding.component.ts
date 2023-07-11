c55258249e577edc1826cf42f57752a1cc091b9a
Bayron Tarazona De La Hoz <bayronthz@gmail.com>
2023-07-10 21:22:04 -0500

    Primer cambio de la clase de binding

---
+++ src/app/clases/binding/binding.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-binding',
+  templateUrl: './binding.component.html',
+  styleUrls: ['./binding.component.css']
+})
+export class BindingComponent implements OnInit {
+
+  constructor() { }
+
+  unaVariable = "holaaaa";
+  unaVariableProperty = true;
+  unaImagen = "https://upload.wikimedia.org/wikipedia/commons/thumb/9/94/Ara_Macao_%28Yarinacocha%2C_Pucallpa%29.jpg/1200px-Ara_Macao_%28Yarinacocha%2C_Pucallpa%29.jpg";
+  unHidding = true;
+  ngOnInit(): void {
+    
+  }
+  retornarMensaje(){
+    return "hola soy un mensaje de función";
+  }
+
+}
